e726431826a161fd2dfe618bcd86e3bf36c5debf
jung-ho-1231 <parkjung-1231@naver.com>
2022-11-14 02:37:09 +0900

    add Member Entity

---
+++ src/main/java/com/example/studyjpa/member/Member.java
@@ -0,0 +1,26 @@
+package com.example.studyjpa.member;
+import com.example.studyjpa.order.Order;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
